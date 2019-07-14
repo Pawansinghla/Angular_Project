a42528f93dede99c2b3d292829a91ca544246f9f
pawan singhla <pawansinghla300@gmail.com>
2019-07-14 18:55:35 +0530

    Dialogs

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,6 +11,7 @@ import {MatListModule} from '@angular/material/list';
 import {MatGridListModule} from '@angular/material/grid-list';
 import {MatCardModule} from '@angular/material/card';
 import {MatButtonModule} from '@angular/material/button';
+import {MatDialogModule} from '@angular/material/dialog';
 import { DishdetailComponent } from './dishdetail/dishdetail.component';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
@@ -22,6 +23,7 @@ import {LeaderService} from './services/leader.service';
 import {DishService} from './services/dish.service';
 import{PromotionService} from './services/promotion.service';
 import {AppRoutingModule} from  './app-routing/app-routing.module';
+import { LoginComponent } from './login/login.component';
 
 @NgModule({
   declarations: [
@@ -32,7 +34,8 @@ import {AppRoutingModule} from  './app-routing/app-routing.module';
     FooterComponent,
     HomeComponent,
     AboutComponent,
-    ContactComponent
+    ContactComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
@@ -44,6 +47,7 @@ import {AppRoutingModule} from  './app-routing/app-routing.module';
     MatGridListModule,
     MatCardModule,
     MatButtonModule,
+    MatDialogModule
   
 
 
@@ -54,6 +58,10 @@ import {AppRoutingModule} from  './app-routing/app-routing.module';
     LeaderService
 
   ],
+  entryComponents: [
+    LoginComponent
+  ],
   bootstrap: [AppComponent]
+
 })
 export class AppModule { }
