859c19b323433c129f4b8983abcd78bf8e750b3e
pawan singhla <pawansinghla300@gmail.com>
2019-07-15 00:32:41 +0530

    Reactive Forms Part1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -22,6 +22,10 @@ import {MatFormFieldModule} from '@angular/material/form-field';
 import {MatInputModule} from '@angular/material/input';
 import {MatCheckboxModule} from '@angular/material/checkbox';
 import {FormsModule} from '@angular/forms';
+import {ReactiveFormsModule} from '@angular/forms';
+import {MatSelectModule} from  '@angular/material/select' ;
+import {MatSlideToggleModule} from '@angular/material/slide-toggle';
+
 import {LeaderService} from './services/leader.service';
 import {DishService} from './services/dish.service';
 import{PromotionService} from './services/promotion.service';
@@ -45,6 +49,8 @@ import { LoginComponent } from './login/login.component';
     BrowserAnimationsModule,
     FlexLayoutModule,
     FormsModule,
+    ReactiveFormsModule,
+    
     AppRoutingModule,
     MatToolbarModule,
     MatListModule,
@@ -54,7 +60,9 @@ import { LoginComponent } from './login/login.component';
     MatDialogModule,
     MatFormFieldModule,
     MatInputModule,
-    MatCheckboxModule
+    MatCheckboxModule,
+    MatSlideToggleModule,
+    MatSelectModule,
 
   
 
--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -1,15 +1,39 @@
 import { Component, OnInit } from '@angular/core';
-
+import { FormBuilder, FormGroup, Validators} from '@angular/forms';
+import { Feedback, ContactType} from '../shared/feedback';
 @Component({
   selector: 'app-contact',
   templateUrl: './contact.component.html',
   styleUrls: ['./contact.component.scss']
 })
 export class ContactComponent implements OnInit {
+ 
+  feedbackForm: FormGroup;
+  feedback: Feedback;
+  contactType = ContactType;
 
-  constructor() { }
+  constructor(private fb: FormBuilder) { 
+  this.createForm();
+  }
 
   ngOnInit() {
+  }
+  createForm(){
+    this.feedbackForm=this.fb.group({
+firstname: '',
+lastname: '',
+telnum: 0,
+email: '',
+agree: false,
+contacttype: 'None',
+message: ''
+
+    });
+  }
+  onSubmit(){
+    this.feedback=this.feedbackForm.value;
+    console.log(this.feedback);
+    this.feedbackForm.reset();
   }
 
 }
